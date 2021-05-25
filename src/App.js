f6d11d49194b8f33d45146ae4ee7cac65af36b41
Derek <78202791+712derek@users.noreply.github.com>
2021-05-25 16:41:50 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import GlobalStyle from "./globalStyles";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+
+      <h1>Hello World!</h1>
+    </>
+  );
+}
+
+export default App;
