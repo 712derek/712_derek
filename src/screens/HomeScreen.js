4c3a7fcf17a73fe8ca1b4fee817ef25c4fd975db
Derek <78202791+712derek@users.noreply.github.com>
2021-06-02 13:27:08 +0200

    effect on background

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,6 +1,24 @@
 import React from "react";
-import styled from "styled-components";
-import Background from "../images/logo_black.png";
+import styled, { keyframes } from "styled-components";
+import BackgroundTop from "../images/logo_black_top.png";
+import BackgroundBottom from "../images/logo_black_bottom.png";
+
+const appearLeft = keyframes`
+  from{
+    transform: translateX(-100%);
+  }
+  to{
+    transform: translateX(0%);
+  }
+`;
+const appearRight = keyframes`
+  from{
+    transform: translateX(100%);
+  }
+  to{
+    transform: translateX(0%);
+  }
+`;
 
 const PageWrapper = styled.section`
   width: 100vw;
@@ -8,17 +26,23 @@ const PageWrapper = styled.section`
   background: #1c1c1a;
 `;
 
-const BackgroundImage = styled.img`
+const BackgroundImageTop = styled.img`
   width: 100vw;
   height: 100vh;
   position: fixed;
   object-fit: cover;
+  animation: ${appearRight} 1.2s cubic-bezier(0.3, 1, 0.3, 1.05);
+`;
+
+const BackgroundImageBottom = styled(BackgroundImageTop)`
+  animation: ${appearLeft} 1.2s cubic-bezier(0.3, 1, 0.3, 1.05);
 `;
 
 const HomeScreen = () => {
   return (
     <PageWrapper>
-      <BackgroundImage src={Background} />
+      <BackgroundImageTop src={BackgroundTop} />
+      <BackgroundImageBottom src={BackgroundBottom} />
     </PageWrapper>
   );
 };
